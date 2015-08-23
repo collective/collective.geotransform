a9d478237850bb32aba892e727313d9517361eef
Martin Peeters <martin.peeters@affinitic.be>
2015-08-23 17:28:55 +0200

    Remove the unneeded whitespaces

--- collective/geotransform/skins/collective_geotransform/geoMailDecoder.js
+++ collective/geotransform/skins/collective_geotransform/geoMailDecoder.js
@@ -1,15 +1,15 @@
-        $(document).ready( function() {
-        	// Decode the geomailaddress span to extract the mail
-        	$("span.geomailaddress").each(function(i){
-                encodedMail = $(this).text();
-                decodedMail = $.base64.decode(encodedMail);
-                $(this).replaceWith(decodedMail);
-            });
-        	// Decode the geomailto href attribute to extract the mail
-        	$("a[href^=geomailto]").each(function(i){
-        		$(this).attr('class', 'link-mailto');
-        		encodedMail = $(this).attr('href').replace("geomailto:","");
-                decodedMail = $.base64.decode(encodedMail);
-        		$(this).attr('href', 'mailto:'+decodedMail);
-        	});
-        });
+$(document).ready( function() {
+	// Decode the geomailaddress span to extract the mail
+	$("span.geomailaddress").each(function(i){
+        encodedMail = $(this).text();
+        decodedMail = $.base64.decode(encodedMail);
+        $(this).replaceWith(decodedMail);
+    });
+	// Decode the geomailto href attribute to extract the mail
+	$("a[href^=geomailto]").each(function(i){
+		$(this).attr('class', 'link-mailto');
+		encodedMail = $(this).attr('href').replace("geomailto:","");
+        decodedMail = $.base64.decode(encodedMail);
+		$(this).attr('href', 'mailto:'+decodedMail);
+	});
+});
